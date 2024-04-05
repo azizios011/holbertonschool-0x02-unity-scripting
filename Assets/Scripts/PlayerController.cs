1f1eec4a3277b8f21eaf8cc86c59fce3028e006e
WhiteWolf <124680639+azizios011@users.noreply.github.com>
2024-04-05 17:55:45 +0100

    You ve activated my trap card

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,35 @@ public class PlayerController : MonoBehaviour {
 
 	public float Speed = 5f; // Public variable to adjust player speed in Inspector
     private int score = 0;
+    public int health = 5;
 
     void OnTriggerEnter(Collider other)
     {
+        // Check if the collided object is tagged as "Pickup".
         if (other.CompareTag("Pickup"))
         {
+            // Increment score.
             score++;
 
+            // Log the updated score to the console
             Debug.Log("Score: " + score);
 
+            // Disable or destroy the coin object
+            // You can choose either option based on your requirements
+            // For example, to disable the coin:
             other.gameObject.SetActive(false);
+            // Or to destroy the coin:
+            // Destroy(other.gameObject);
+        }
+        // Check if the collided object is tagged as "Trap".
+        else if (other.CompareTag("Trap"))
+        {
+            // Decrement health.
+            health--;
+
+            // Log the updated health to the console.
+            Debug.Log("Health: " + health);
+            // Also we can add logic here to handle player death if health reaches 0
         }
     }
     // Update is called once per frame
